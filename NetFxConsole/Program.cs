6313740f65ab13a09586aa1832b63b4c27fff83b
Mark Healy <mark.healy@employmenthero.com>
2024-11-07 10:25:23 +1030

    added net framework console app

---
+++ NetFxConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NetFxConsole
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            LibraryA_Standard20.Test.RunTest();
+        }
+    }
+}
